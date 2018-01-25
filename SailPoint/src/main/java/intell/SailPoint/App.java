572df681fea7b0e6b894ad5430149790c23ef4ee
maddyverma9 <maddyverma9@gmail.com>
2018-01-24 16:22:28 -0800

    new Project Created

---
+++ SailPoint/src/main/java/intell/SailPoint/App.java
@@ -0,0 +1,13 @@
+package intell.SailPoint;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
